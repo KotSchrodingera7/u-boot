c879ab8adc4a7cfaffbda143cfb975e4f9dd0c7b
Alex <skupov93@gmail.com>
2025-06-23 17:55:34 +0500

    rk3588: Added config for new board of diasom.

---
+++ include/configs/diasom-rk3588.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2022 Collabora Ltd.
+ */
+
+#ifndef __DIASOM_RK3588_H
+#define __DIASOM_RK3588_H
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#include <configs/rk3588_common.h>
+
+#endif /* __DIASOM_RK3588_H */
